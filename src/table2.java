28225c572516ff8bb620a29c45b0a7a046fcbcbb
jaskahlon <jesskahlon83@gmail.com>
2022-05-02 17:22:20 +0100

    first commit

--- src/table2.java
+++ src/table2.java
@@ -1,12 +1,11 @@
 public class table2 {
-    public static void main(String[]args){
-        int num =2, i=1;
-        while(i<=10)
-        {
+    public static void main(String[] args) {
+        int num = 2, i = 1;
+        while (i <= 10) {
             System.out.printf("%d * %d = %d \n", num, i, num * i);
             i++;
 
         }
     }
+
 }
-//homework
